5c7949c3dc968c89f7a529828bd50e68fe479df7
Brian Paul <brompo@gmail.com>
2026-03-17 15:32:58 +0300

    minor cosmetic adjustements

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.on11r6s5scg"
+    "revision": "0.lirk0db6hj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
