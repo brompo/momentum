d7e15bdda3d660095302319b2e4991c7b553e2be
Brian Paul <brompo@gmail.com>
2026-04-05 17:36:20 +0300

    FIX Errors at the Vision Page

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.e7g19bg8d7o"
+    "revision": "0.3lij4ieimbg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
