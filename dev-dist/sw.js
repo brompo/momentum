a5a29777e9bb602fb75b0ee1d6f1e9af4a4120fd
Brian Paul <brompo@gmail.com>
2026-04-07 10:17:32 +0300

    fix: remove redundant 110px padding from milestones-modern-grid to fix weird space

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-7a5e81cd'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.hvc4a8308ng"
+    "revision": "0.j3nkb8aho4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
