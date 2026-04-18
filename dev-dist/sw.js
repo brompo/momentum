7341d99d2834c56486439496b6752a623148b9de
Brian Paul <brompo@gmail.com>
2026-04-18 07:16:28 +0300

    Minor Changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.fjt8m8hedmo"
+    "revision": "0.sk3r5dfuq0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
